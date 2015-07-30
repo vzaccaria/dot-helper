4d3e312c11c6001c9981121b53892ff3a593ae74
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-07-30 11:52:25 +0200

    feat: generates array of structures boilerplate

--- index.js
+++ index.js
@@ -5,26 +5,39 @@
 var _require = require("zaccaria-cli");
 
 var $d = _require.$d;
-var getOption = _require.getOption;
+var $o = _require.$o;
 var $f = _require.$f;
+var $b = _require.$b;
+
+var $c = $b.promisify(require("copy-paste").copy);
 
 var getOptions = function (doc) {
     "use strict";
     var o = $d(doc);
-    var help = getOption("-h", "--help", false, o);
+    var help = $o("-h", "--help", false, o);
+    var vname = o.VARNAME;
+    var sname = $o("-s", "--structure", null, o);
+    var num = $o("-n", "--number", "0", o);
+    var it = $o("-i", "--iterator", "i", o);
+    var max = $o("-m", "--max", "?", o);
     return {
-        help: help
+        help: help, vname: vname, sname: sname, num: num, it: it, max: max
     };
+};
+
+var template = function (d) {
+    return "\n\tdigraph g {\n\n    graph [\n        rankdir = \"LR\"\n    ];\n\n\tsubgraph cluster_" + d.num + "0 {\n        label = \"" + d.vname + "\";\n        fontname = \"Roboto Condensed Regular\"\n        \"node" + d.num + "00\" [\n            label = \"[0]|...| <ty" + d.num + "> [" + d.it + "] |...| [" + d.max + "]\"\n            shape = \"record\"\n            fontname = \"Roboto Condensed Regular\"\n         ];\n         \"node" + d.num + "01\" [\n            label=\"Array di '" + d.sname + "'\"\n            shape=plaintext\n            fontname=\"Roboto Condensed Bold\"\n         ]\n    }\n\n\tsubgraph cluster_" + d.num + "1 {\n        label = \"" + d.vname + "[" + d.it + "]\";\n        fontname = \"Roboto Condensed Regular\"\n        \"node" + d.num + "10\" [\n            label = \"f1 | f2 | f3\"\n            shape = \"record\"\n                        fontname = \"Roboto Condensed Regular\"\n            ];\n\n        \"node" + d.num + "11\" [\n            label=\"struct '" + d.sname + "'\"\n            shape=plaintext\n            fontname=\"Roboto Condensed Bold\"\n        ]\n    }\n\n\tnode" + d.num + "00:ty" + d.num + " -> node" + d.num + "10:nw\n}\n\t";
 };
 
 var main = function () {
     $f.readLocal("docs/usage.md").then(function (it) {
-        var _getOptions = getOptions(it);
-
-        var help = _getOptions.help;
-
-        if (help) {
+        var o = getOptions(it);
+        if (o.help) {
             console.log(it);
+        } else {
+            $c(template(o)).then(function () {
+                console.log("Copied");
+            });
         }
     });
 };
--- src/index.js
+++ src/index.js
@@ -1,25 +1,77 @@
 /* eslint quotes: [0], strict: [0] */
 var {
-    $d, getOption, $f
+    $d, $o, $f, $b
 } = require('zaccaria-cli')
+
+var $c = $b.promisify(require('copy-paste').copy)
 
 var getOptions = doc => {
     "use strict"
     var o = $d(doc)
-    var help = getOption('-h', '--help', false, o)
+    var help	= $o('-h', '--help', false, o)
+	var vname	= o.VARNAME
+	var sname	= $o('-s', '--structure', null, o)
+	var num		= $o('-n', '--number', '0', o)
+	var it		= $o('-i', '--iterator', 'i', o)
+	var max		= $o('-m', '--max', '?', o)
     return {
-        help
+        help, vname, sname, num, it, max
     }
 }
 
+var template = (d) => {
+	return `
+	digraph g {
+
+    graph [
+        rankdir = "LR"
+    ];
+
+	subgraph cluster_${d.num}0 {
+        label = "${d.vname}";
+        fontname = "Roboto Condensed Regular"
+        "node${d.num}00" [
+            label = "[0]|...| <ty${d.num}> [${d.it}] |...| [${d.max}]"
+            shape = "record"
+            fontname = "Roboto Condensed Regular"
+         ];
+         "node${d.num}01" [
+            label="Array di '${d.sname}'"
+            shape=plaintext
+            fontname="Roboto Condensed Bold"
+         ]
+    }
+
+	subgraph cluster_${d.num}1 {
+        label = "${d.vname}[${d.it}]";
+        fontname = "Roboto Condensed Regular"
+        "node${d.num}10" [
+            label = "f1 | f2 | f3"
+            shape = "record"
+            fontname = "Roboto Condensed Regular"
+            ];
+
+        "node${d.num}11" [
+            label="struct '${d.sname}'"
+            shape=plaintext
+            fontname="Roboto Condensed Bold"
+        ]
+    }
+
+	node${d.num}00:ty${d.num} -> node${d.num}10:nw
+}
+	`}
+
 var main = () => {
     $f.readLocal('docs/usage.md').then(it => {
-        var {
-            help
-        } = getOptions(it);
-        if (help) {
+        var o = getOptions(it);
+        if (o.help) {
             console.log(it)
-        }
+        } else {
+		$c(template(o)).then( () => {
+				console.log("Copied");
+			})
+		}
     })
 }
 
